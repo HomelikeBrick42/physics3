60772505ede9b52d2173d07ec860f0ebaab5b020
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2023-03-29 11:01:53 +1300

    Weird crash when circle 14 touches the floor

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,48 +4,52 @@ use physics::{
 };
 use raylib::prelude::*;
 
+fn random_in_range(rng: &mut dyn rand::RngCore, range: std::ops::RangeInclusive<Number>) -> Number {
+    use rand::Rng;
+
+    let start = range.start().to_bits();
+    let end = range.end().to_bits();
+    let result = rng.gen_range(start..=end);
+    Number::from_bits(result)
+}
+
 fn main() {
     let (mut rl, thread) = raylib::init()
         .title("Hello, World")
         .size(WINDOW_WIDTH as _, WINDOW_HEIGHT as _)
         .msaa_4x()
+        .vsync()
         .build();
 
+    let min_bounds = Vector2 {
+        x: -Number::from_num(WINDOW_WIDTH) / number!(2),
+        y: -Number::from_num(WINDOW_HEIGHT) / number!(2),
+    };
+    let max_bounds = Vector2 {
+        x: Number::from_num(WINDOW_WIDTH) / number!(2),
+        y: Number::from_num(WINDOW_HEIGHT) / number!(2),
+    };
+
+    let mut rng = <rand::rngs::StdRng as rand::SeedableRng>::from_seed(Default::default());
     let mut state = PhysicsState::new(
-        Some(Vector2 {
-            x: -Number::from_num(WINDOW_WIDTH) / number!(2),
-            y: -Number::from_num(WINDOW_HEIGHT) / number!(2),
-        }),
-        Some(Vector2 {
-            x: Number::from_num(WINDOW_WIDTH) / number!(2),
-            y: Number::from_num(WINDOW_HEIGHT) / number!(2),
-        }),
-        [
+        Some(min_bounds),
+        Some(max_bounds),
+        std::iter::repeat_with(|| {
+            let radius = random_in_range(&mut rng, number!(10.0)..=number!(20.0));
             Circle {
                 position: Vector2 {
-                    x: number!(-100),
-                    y: number!(0),
+                    x: random_in_range(&mut rng, (min_bounds.x + radius)..=(max_bounds.x - radius)),
+                    y: random_in_range(&mut rng, (min_bounds.y + radius)..=(max_bounds.x - radius)),
                 },
                 velocity: Vector2 {
-                    x: number!(50),
-                    y: number!(0),
+                    x: random_in_range(&mut rng, number!(-50.0)..=number!(50.0)),
+                    y: random_in_range(&mut rng, number!(-50.0)..=number!(50.0)),
                 },
-                mass: number!(2),
-                radius: number!(50),
-            },
-            Circle {
-                position: Vector2 {
-                    x: number!(200),
-                    y: number!(-30),
-                },
-                velocity: Vector2 {
-                    x: number!(-50),
-                    y: number!(0),
-                },
-                mass: number!(1),
-                radius: number!(50),
-            },
-        ],
+                mass: Number::PI * radius * radius,
+                radius,
+            }
+        })
+        .take(50),
     );
     let mut last_time = std::time::Instant::now();
     let mut fixed_time = number!(0);
@@ -56,9 +60,16 @@ fn main() {
 
         let dt = Number::from_num(dt.as_nanos()) * number!(0.000_000_001);
         fixed_time += dt;
-        while fixed_time >= TIME_STEP {
+        // while fixed_time >= TIME_STEP {
+        //     state.update(TIME_STEP);
+        //     fixed_time -= TIME_STEP;
+        // }
+
+        if rl.is_key_pressed(KeyboardKey::KEY_F) {
             state.update(TIME_STEP);
-            fixed_time -= TIME_STEP;
+        }
+        if rl.is_key_down(KeyboardKey::KEY_SPACE) {
+            state.update(TIME_STEP);
         }
 
         // Rendering
@@ -80,14 +91,14 @@ fn main() {
                     rotation: 0.0,
                     zoom: 1.0,
                 });
-                for circle in &state.circles {
+                for (index, circle) in state.circles.iter().enumerate() {
                     d.draw_circle_v(
                         raylib::math::Vector2 {
                             x: circle.position.x.lossy_into(),
                             y: (-circle.position.y).lossy_into(),
                         },
                         circle.radius.lossy_into(),
-                        Color::RED,
+                        if index == 14 { Color::BLUE } else { Color::RED },
                     );
                 }
             }
--- src/state.rs
+++ src/state.rs
@@ -29,114 +29,140 @@ impl PhysicsState {
     }
 
     pub fn update(&mut self, ts: Number) {
+        self.circles
+            .par_iter_mut()
+            .for_each(|circle| circle.velocity.y -= number!(200) * ts);
+
         let solved = AtomicBool::new(false);
-        while !solved.load(Relaxed) {
+        for _ in 0..1000 {
+            if solved.load(Relaxed) {
+                break;
+            }
             solved.store(true, Relaxed);
 
             std::mem::swap(&mut self.circles, &mut self.old_circles);
             self.circles.clear();
             self.circles
-                .par_extend(self.old_circles.par_iter().copied().enumerate().map(
-                    |(index, mut circle)| {
-                        'collision_checks: {
-                            if let Some(min_bounds) = self.min_bounds {
-                                if (circle.velocity.y < Number::ZERO)
-                                    && (circle.position.y - circle.radius < min_bounds.y)
-                                {
-                                    let depth = (circle.position.y - circle.radius) - min_bounds.y;
-                                    let time = depth / -circle.velocity.y;
-
-                                    // rewind time
-                                    circle.position -= circle.velocity * time;
-                                    // adjust velocity
-                                    circle.velocity.y = -circle.velocity.y;
-                                    // step back foward through time
-                                    circle.position += circle.velocity * time;
-
-                                    break 'collision_checks;
-                                }
-
-                                if (circle.velocity.x < Number::ZERO)
-                                    && (circle.position.x - circle.radius < min_bounds.x)
-                                {
-                                    let depth = (circle.position.x - circle.radius) - min_bounds.x;
-                                    let time = depth / -circle.velocity.x;
-
-                                    // rewind time
-                                    circle.position -= circle.velocity * time;
-                                    // adjust velocity
-                                    circle.velocity.x = -circle.velocity.x;
-                                    // step back foward through time
-                                    circle.position += circle.velocity * time;
-
-                                    break 'collision_checks;
-                                }
+                .extend(
+                    self.old_circles
+                        .iter()
+                        .copied()
+                        .enumerate()
+                        .map(|(index, mut circle)| {
+                            if circle.position.sqr_length() > number!(1000000) {
+                                println!("{index}: {:?} {:?}", circle.position, circle.velocity);
                             }
 
-                            if let Some(max_bounds) = self.max_bounds {
-                                if (circle.velocity.y > Number::ZERO)
-                                    && (circle.position.y + circle.radius > max_bounds.y)
-                                {
-                                    let depth = (circle.position.y + circle.radius) - max_bounds.y;
-                                    let time = depth / circle.velocity.y;
-
-                                    // rewind time
-                                    circle.position -= circle.velocity * time;
-                                    // adjust velocity
-                                    circle.velocity.y = -circle.velocity.y;
-                                    // step back foward through time
-                                    circle.position += circle.velocity * time;
-
-                                    break 'collision_checks;
-                                }
-
-                                if (circle.velocity.x > Number::ZERO)
-                                    && (circle.position.x + circle.radius > max_bounds.x)
-                                {
-                                    let depth = (circle.position.x + circle.radius) - max_bounds.x;
-                                    let time = depth / circle.velocity.x;
-
-                                    // rewind time
-                                    circle.position -= circle.velocity * time;
-                                    // adjust velocity
-                                    circle.velocity.x = -circle.velocity.x;
-                                    // step back foward through time
-                                    circle.position += circle.velocity * time;
-
-                                    break 'collision_checks;
-                                }
-                            }
-
-                            for (other_index, other_circle) in self.old_circles.iter().enumerate() {
-                                if index != other_index {
-                                    let relative_velocity = circle.velocity - other_circle.velocity;
-                                    let relative_position = circle.position - other_circle.position;
-                                    let sqr_distance = relative_position.sqr_length();
-                                    let combined_radius = circle.radius + other_circle.radius;
-                                    if (sqr_distance < combined_radius * combined_radius)
-                                        && (relative_position.dot(-relative_velocity)
-                                            > Number::ZERO)
+                            'collision_checks: {
+                                if let Some(min_bounds) = self.min_bounds {
+                                    if (circle.velocity.y < Number::ZERO)
+                                        && (circle.position.y - circle.radius < min_bounds.y)
                                     {
-                                        // TODO: find collision time so position can be adjusted
+                                        let depth =
+                                            (circle.position.y - circle.radius) - min_bounds.y;
+                                        let time = depth / -circle.velocity.y;
 
-                                        circle.velocity -= ((number!(2) * other_circle.mass)
-                                            / (circle.mass + other_circle.mass))
-                                            * (relative_velocity.dot(relative_position)
-                                                / sqr_distance)
-                                            * relative_position;
+                                        // rewind time
+                                        circle.position -= circle.velocity * time;
+                                        // adjust velocity
+                                        circle.velocity.y = circle.velocity.y.abs();
+                                        // step back foward through time
+                                        circle.position += circle.velocity * time;
+
+                                        break 'collision_checks;
+                                    }
+
+                                    if (circle.velocity.x < Number::ZERO)
+                                        && (circle.position.x - circle.radius < min_bounds.x)
+                                    {
+                                        let depth =
+                                            (circle.position.x - circle.radius) - min_bounds.x;
+                                        let time = depth / -circle.velocity.x;
+
+                                        // rewind time
+                                        circle.position -= circle.velocity * time;
+                                        // adjust velocity
+                                        circle.velocity.x = circle.velocity.x.abs();
+                                        // step back foward through time
+                                        circle.position += circle.velocity * time;
 
                                         break 'collision_checks;
                                     }
                                 }
+
+                                if let Some(max_bounds) = self.max_bounds {
+                                    if (circle.velocity.y > Number::ZERO)
+                                        && (circle.position.y + circle.radius > max_bounds.y)
+                                    {
+                                        let depth =
+                                            (circle.position.y + circle.radius) - max_bounds.y;
+                                        let time = depth / circle.velocity.y;
+
+                                        // rewind time
+                                        circle.position -= circle.velocity * time;
+                                        // adjust velocity
+                                        circle.velocity.y = -circle.velocity.y.abs();
+                                        // step back foward through time
+                                        circle.position += circle.velocity * time;
+
+                                        break 'collision_checks;
+                                    }
+
+                                    if (circle.velocity.x > Number::ZERO)
+                                        && (circle.position.x + circle.radius > max_bounds.x)
+                                    {
+                                        let depth =
+                                            (circle.position.x + circle.radius) - max_bounds.x;
+                                        let time = depth / circle.velocity.x;
+
+                                        // rewind time
+                                        circle.position -= circle.velocity * time;
+                                        // adjust velocity
+                                        circle.velocity.x = -circle.velocity.x.abs();
+                                        // step back foward through time
+                                        circle.position += circle.velocity * time;
+
+                                        break 'collision_checks;
+                                    }
+                                }
+
+                                for (other_index, other_circle) in
+                                    self.old_circles.iter().enumerate()
+                                {
+                                    if index != other_index {
+                                        let relative_velocity =
+                                            circle.velocity - other_circle.velocity;
+                                        let relative_position =
+                                            circle.position - other_circle.position;
+                                        let sqr_distance = relative_position.sqr_length();
+                                        let combined_radius = circle.radius + other_circle.radius;
+                                        if (sqr_distance < combined_radius * combined_radius)
+                                            && (relative_position.dot(-relative_velocity)
+                                                > Number::ZERO)
+                                        {
+                                            // TODO: find collision time so position can be adjusted
+
+                                            circle.velocity -= ((number!(2) * other_circle.mass)
+                                                / (circle.mass + other_circle.mass))
+                                                * (relative_velocity.dot(relative_position)
+                                                    / sqr_distance)
+                                                * relative_position;
+
+                                            break 'collision_checks;
+                                        }
+                                    }
+                                }
+
+                                return circle;
                             }
 
-                            return circle;
-                        }
-
-                        solved.store(false, Relaxed);
-                        circle
-                    },
-                ));
+                            if index == 14 {
+                                println!("Circle 14 collision");
+                            }
+                            solved.store(false, Relaxed);
+                            circle
+                        }),
+                );
         }
         self.circles
             .par_iter_mut()
